b03a79b5e3bed259cfab00d1508fdb5155ae0fa0
mintubairwa20 <mintubairwa20@gmail.com>
2026-07-28 19:20:12 +0530

    fixed axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -25,14 +25,32 @@
  *     never need to be reconfigured per-module.
  */
 
+// import axios from "axios";
+
+
+// console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+
+//   // CRITICAL: sends HttpOnly cookies (accessToken, refreshToken) automatically
+//   withCredentials: true,
+
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
+
 import axios from "axios";
+
+console.log("ENV CHECK:", import.meta.env.VITE_API_URL);
+console.log("ALL ENV:", import.meta.env);
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-
-  // CRITICAL: sends HttpOnly cookies (accessToken, refreshToken) automatically
   withCredentials: true,
-
   headers: {
     "Content-Type": "application/json",
   },
